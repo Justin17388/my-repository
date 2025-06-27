001b1326b017b8bc8896733a7a74576a51e9e028
Justin Shinn <jmshinn13@yahoo.com>
2025-06-26 20:49:48 -0500

    Second commit

---
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+@app.route("/bye")
+def theend():
+    return "<h1>Good Bye. Come Again</h1>"
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
